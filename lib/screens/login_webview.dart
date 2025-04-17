f501ca9ba5eb8d545eda98071b12fb7b397b14b4
Wojciech Sekuła <wojciech.sekula.kontakt.@gmail.com>
2025-04-17 11:32:20 +0200

    working token, errors in checkSession

--- lib/screens/login_webview.dart
+++ lib/screens/login_webview.dart
@@ -13,9 +13,7 @@ class LoginWebView extends StatefulWidget {
 class _LoginWebViewState extends State<LoginWebView> {
 	late InAppWebViewController _webViewController;
 
-	// Adres, na który serwer przekierowuje po zalogowaniu
 	static const _successUrlPrefix = 'https://info-car.pl/new/';
-	static const _nextPageUrl = 'https://info-car.pl/new/prawo-jazdy/sprawdz-wolny-termin';
 
 	@override
 	Widget build(BuildContext context) {
@@ -30,23 +28,22 @@ class _LoginWebViewState extends State<LoginWebView> {
 			(InAppWebViewController controller, WebResourceRequest request) async {
 			final url = request.url.toString();
 			if (url.contains('/api/word/word-centers')) {
-			print('🕵️‍♂️ Intercepted request to word-centers');
-			print('Method: ${request.method}');
-			print('Headers:');
-			request.headers?.forEach((key, value) {
-				print('  $key: $value');
-			});
+				if (request.headers?.containsKey("Authorization") ?? false) {
+				final authorizationHeader = request.headers?["Authorization"];
+				if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+				final token = authorizationHeader.substring(7); // Pobieramy token bez "Bearer "
+					GlobalVars.bearerToken = token;
+					//print("token = " + token);
+					Navigator.pop(context);
+				}
+				}
 			}
-			// zwracamy null, żeby WebView kontynuowało normalne ładowanie
 			return null;
 		},
 
 		onLoadStop: (controller, uri) async {
 			final url = uri?.toString() ?? '';
-			// Sprawdzamy, czy URL zaczyna się od _successUrlPrefix (po zalogowaniu)
 			if (url.startsWith(_successUrlPrefix)) {
-				// Gdy użytkownik jest zalogowany, uruchamiamy JS do kliknięcia w link
-				print('User logged in, now clicking the link...');
 				await controller.evaluateJavascript(source: """
 				const link = document.querySelector('a[href="/new/prawo-jazdy/sprawdz-wolny-termin"]');
 				if (link) {
